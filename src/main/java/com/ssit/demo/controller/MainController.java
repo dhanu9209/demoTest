bcc3f50bbe99583fa2cb59e4e7d69a581fe1b599
unknown <dhanu9209@gmail.com>
2021-02-07 10:27:06 +0530

    Main Controller some changes added

--- src/main/java/com/ssit/demo/controller/MainController.java
+++ src/main/java/com/ssit/demo/controller/MainController.java
@@ -1,5 +1,13 @@
 package com.ssit.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
 public class MainController {
+	@GetMapping
+	public String helloword() {
+		return "Hello World";
+	}
 
 }
