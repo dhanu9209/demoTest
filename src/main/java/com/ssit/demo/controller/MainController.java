2fa1fdba2e618a139e87b7bf178e18936d4b73c7
unknown <dhanu9209@gmail.com>
2021-02-07 19:59:18 +0530

    employee controller added

--- src/main/java/com/ssit/demo/controller/MainController.java
+++ src/main/java/com/ssit/demo/controller/MainController.java
@@ -2,7 +2,12 @@ package com.ssit.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/**
+ * 
+ * @author dhanu
+ *
+ *
+ */
 @RequestMapping("/")
 public class MainController {
 	@GetMapping
